f5e7b4d14c0cc3dc90702aeb33227f0fe3bbb201
Andrew Vasilchenko <cazonove@gmail.com>
2023-12-15 22:48:01 +0300

    added page

--- app/js/app.js
+++ app/js/app.js
@@ -16,8 +16,8 @@ document.addEventListener('DOMContentLoaded', () => {
 		const data = e.clipboardData.getData('text')
 		const value = data.split('')
 
-		if(value.length === emailCodeInputs.length) {
-			emailCodeInputs.forEach((input, i) => {input.value = value[i]})
+		if (value.length === emailCodeInputs.length) {
+			emailCodeInputs.forEach((input, i) => { input.value = value[i] })
 			emailCodeSubmit()
 		}
 	}
@@ -25,20 +25,20 @@ document.addEventListener('DOMContentLoaded', () => {
 	function handleCode(e) {
 		const input = e.target;
 		const value = input.value;
-	
+
 		input.value = '';
 		input.value = value ? value[0] : '';
-	
+
 		const fieldIndex = parseInt(input.dataset.index); // преобразуем в число
-	
+
 		if (value.length > 0 && fieldIndex < emailCodeInputs.length - 1) {
 			emailCodeInputs[fieldIndex + 1]?.focus(); // проверяем, что элемент существует перед вызовом focus()
 		}
-	
+
 		if (e.key === 'Backspace' && fieldIndex > 0) {
 			emailCodeInputs[fieldIndex - 1]?.focus(); // проверяем, что элемент существует перед вызовом focus()
 		}
-	
+
 		if (fieldIndex === emailCodeInputs.length - 1) {
 			emailCodeSubmit();
 		}
@@ -53,12 +53,36 @@ document.addEventListener('DOMContentLoaded', () => {
 	const backButtons = document.querySelectorAll('.btn-back');
 
 	if (backButtons.length > 0) {
-		backButtons.forEach(function(button) {
-			button.addEventListener('click', function() {
+		backButtons.forEach(function (button) {
+			button.addEventListener('click', function () {
 				history.back();
 			});
 		});
 	}
 	//** (End) Buttons Back **/
+
+	//** (Start) Custom Display Maxlength **/
+	const maxLengthInputBlocks = document.querySelectorAll('[data-custom-input-maxlength="true"]');
+
+	if (maxLengthInputBlocks.length > 0) {
+		maxLengthInputBlocks.forEach(function (block) {
+			const input = block.querySelector('input');
+			const display = block.querySelector('[data-custom-display-maxlength="true"]');
+
+			if (input && display) {
+				const maxLength = input.getAttribute('maxlength');
+
+				const updateCharacterCount = function () {
+					const currentLength = input.value.length;
+					display.textContent = `${currentLength}/${maxLength}`;
+				};
+
+				input.addEventListener('input', updateCharacterCount);
+
+				updateCharacterCount();
+			}
+		});
+	}
+	//** (End) Custom Display Maxlength **/
 
 })
